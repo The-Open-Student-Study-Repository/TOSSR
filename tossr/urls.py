2e8f8a8df21e9949128eb7a958d9518b1caf1e71
Matthew <matthewjamesmacphaildev@gmail.com>
2026-03-16 23:56:13 +0000

    28 create backend for searching for modules (#44)
    * Created back end for searching for modules and wrote tests for searching for modules.
    
    * Updated back end to be able to filter by school, credits and level.
    
    ---------
    
    Co-authored-by: Matthew MacPhail <2996230m@glasgow.ac.uk>

--- tossr/urls.py
+++ tossr/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# import the include
 from django.urls import path, include
 from django.shortcuts import redirect
 
@@ -25,4 +24,5 @@ urlpatterns = [
     path('materials/', include('materials.urls')),
         path('accounts/', include('accounts.urls')),
         path('', lambda request: redirect('accounts:login')),
+        path('modules/', include('modules.urls')),
 ]
