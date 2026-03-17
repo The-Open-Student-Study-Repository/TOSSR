7ab32d412e969b9b257b718a3f21fad4efa8eec0
Matthew MacPhail <2996230m@glasgow.ac.uk>
2026-03-17 13:00:02 +0000

    Updated staic file path for pythonanywhere

--- tossr/settings.py
+++ tossr/settings.py
@@ -141,7 +141,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Custom user model
 AUTH_USER_MODEL = 'accounts.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
