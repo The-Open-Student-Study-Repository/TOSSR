5920dfcb4c9aedfbbbe9afe0b31717b47bd64372
Kai Loach <3002935l@student.gla.ac.uk>
2026-03-26 19:44:02 +0000

    fixed the modules link in sidebar

--- modules/templatetags/modules_template_tags.py
+++ modules/templatetags/modules_template_tags.py
@@ -1,9 +1,24 @@
 from django import template
-from modules.models import PinnedModule
+from modules.models import PinnedModule,StudentModule
 
 register = template.Library()
 
 @register.inclusion_tag('modules/starred.html')
-def get_starred_modules_list(current_module=None):
-    return {'starred': PinnedModule.objects.all()[:5],
+def get_starred_modules_list(user,current_module=None):
+
+    user_subscriptions = set()
+    user_favourites = set()
+    if user.is_authenticated and user.role == 'student':
+        student = user.student_profile
+        user_subscriptions = set(
+            StudentModule.objects.filter(student=student).values_list('module_id', flat=True)
+        )
+        user_favourites = set(
+            PinnedModule.objects.filter(student=student)
+        )
+    for module in user_favourites:
+        module.is_subscribed = module.module.id in user_subscriptions
+
+    
+    return {'starred': list(user_favourites)[:5],
             'current_module': current_module}
